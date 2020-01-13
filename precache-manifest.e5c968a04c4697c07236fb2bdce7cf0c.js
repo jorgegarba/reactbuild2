c5c3dd7339821b16f6cabd37d5612167c558cf9d
Jorge Garnica <jorgegarba@gmail.com>
2020-01-13 09:55:19 -0500

    inicio

---
+++ precache-manifest.e5c968a04c4697c07236fb2bdce7cf0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db77a5e4b271f1028381ff5b3ebf7e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9d62b7941906385bb41",
+    "url": "/static/js/2.72f65cb5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.72f65cb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d75997f09275d394f774",
+    "url": "/static/js/main.21fd643e.chunk.js"
+  },
+  {
+    "revision": "d9b853db11ee6d1b2ff2",
+    "url": "/static/js/runtime-main.9cc02061.js"
+  }
+]);
